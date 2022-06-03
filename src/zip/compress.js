edef6798b3f7220a17858112a8aab5db64d97e02
Дмитрий Земляков <dmitriyz@Mac-mini-Dmitrij.local>
2022-06-03 17:21:04 +0300

    feat: Complete transform stream

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,26 @@
+import fs from "fs";
+import zlib from "zlib";
+import { pipeline } from "stream";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const dirPath = path.join(__dirname, "/files/");
+
+let fileName = "fileToCompress.txt";
+
 export const compress = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(dirPath + fileName);
+  const writeStream = fs.createWriteStream(dirPath + fileName + ".gz");
+  const gzip = zlib.createGzip();
+
+  pipeline(readStream, gzip, writeStream, (err) => {
+    if (err) {
+      console.error("Pipeline failed.", err);
+    } else {
+      console.log("success!");
+    }
+  });
 };
+compress();
