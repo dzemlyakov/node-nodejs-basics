53c556bb71d7dbf3c20f50a55174e2d2e51deba0
Дмитрий Земляков <dmitriyz@Mac-mini-Dmitrij.local>
2022-06-03 15:44:39 +0300

    feat: implement reading and writing streams in task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,19 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const dirPath = path.join(__dirname, "/files/");
+
+let fileName = "fileToRead.txt";
+
 export const read = async () => {
-    // Write your code here 
+  const readStream = fs.createReadStream(dirPath + fileName);
+  const writeToTerminal = process.stdout;
+
+  readStream.pipe(writeToTerminal);
+
+  readStream.on("data", () => {});
 };
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,23 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const dirPath = path.join(__dirname, "/files/");
+
+let fileName = "fileToWrite.txt";
+
 export const write = async () => {
-    // Write your code here 
+  const writeStream = fs.createWriteStream(dirPath + fileName);
+  const readStreamFromTerminal = process.stdin;
+
+  readStreamFromTerminal.pipe(writeStream);
+
+  readStreamFromTerminal.on("data", (data) => {
+    if (data.toString().match("exit")) {
+      readStreamFromTerminal.unpipe(writeStream);
+    }
+  });
 };
+write();
