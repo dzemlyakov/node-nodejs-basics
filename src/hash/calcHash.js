992c1e4945429cadbe69b46f171bee84e0cac622
Дмитрий Земляков <dmitriyz@Mac-mini-Dmitrij.local>
2022-06-03 08:41:53 +0300

    feat: done hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,27 @@
+import fs from "fs";
+import crypto from "crypto";
+
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const dirPath = path.join(__dirname, "/files/");
+
+let fileName = "fileToCalculateHashFor.txt";
+
 export const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const fileToHash = await fs.promises.readFile(dirPath + fileName, {
+      encoding: "utf8",
+    });
+    const hashSum = await crypto.createHash("sha256");
+    await hashSum.update(fileToHash);
+    const hex = await hashSum.digest("hex");
+
+    console.log(hex);
+  } catch (err) {
+    console.log(err.message);
+  }
 };
+calculateHash();
