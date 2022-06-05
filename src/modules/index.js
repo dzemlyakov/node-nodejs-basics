0fa4699be7adf6a28b6d81abba9a601ddabc8550
Дмитрий Земляков <dmitriyz@Mac-mini-Dmitrij.local>
2022-06-05 17:05:14 +0300

    refactor: npm scripts and fixes

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,5 +2,5 @@ import {unknownObject, createMyServer} from './cjsToEsm.mjs'
 
 console.log(unknownObject);
 createMyServer.listen(3000, 'localhost', ()=> {
-    console.log('port');
+    console.log('Go to localhost:3000 \n\npress ctrl-c to exit ');
 })
